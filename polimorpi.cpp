9a5c48d90710fc266e959a5f63de5d4c46a80c16
anandajuliar <anandajulia001@gmail.com>
2024-05-28 17:50:58 +0700

    main code

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -22,3 +22,17 @@ public:
         cout << "Pesan dari lia" << endl;
     }
 };
+
+int main() {
+    seseorang*objek;
+    joko a;
+    lia b;
+
+    objek = &a;
+    objek->pesan();
+    objek = &b;
+    objek->pesan();
+    //a.seseorang::pesan();
+
+    return 0;
+}
