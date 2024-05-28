e4e55dd7a31fff6d777c080aaa958ed2c80e7603
anandajuliar <anandajulia001@gmail.com>
2024-05-28 18:30:08 +0700

    int main

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,18 @@ class remotelampu {
             return saklarNo[i];
         }
 };
+
+int main() {
+    remotelampu lampuRumah;
+
+    lampuRumah.setsaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setsaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setsaklarNo(2, "Lampu kamar Tidur");
+    lampuRumah.setsaklarNo(3, "Lampu Dapur");
+
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+    return 0;
+}
